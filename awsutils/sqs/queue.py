d0345b1af170811a7c9f1b18c13c6aab70ba2ec0
sanyi <sanyi@fake.com>
2013-02-02 00:06:55 +0200

    + added message pooling generator - code formating

--- awsutils/sqs/queue.py
+++ awsutils/sqs/queue.py
@@ -4,7 +4,7 @@
 # This module is part of awsutils and is released under
 # the MIT License: http://www.opensource.org/licenses/mit-license.php
 
-import time
+import time, logging
 from awsutils.exceptions.aws import UserInputException
 from awsutils.sqs.message import SQSMessage
 
@@ -20,6 +20,8 @@ class SQSQueue:
         """
         self.qName = qName
         self.sqsservice = sqsservice
+        self.logger = logging.getLogger("%s.%s" % (type(self).__module__, type(self).__name__))
+        self.logger.addHandler(logging.NullHandler())
 
     def refresh(self):
         """
@@ -64,14 +66,16 @@ class SQSQueue:
                 if _waitTimeSeconds <= 0:
                     return None
                 print(time.time(), waitTimeSeconds, _waitTimeSeconds)
-                messages = self.sqsservice.sqsclient.receiveMessage(self.qName, attributes, maxNumberOfMessages,                                                                    visibilityTimeout, _waitTimeSeconds)
+                messages = self.sqsservice.sqsclient.receiveMessage(self.qName, attributes, maxNumberOfMessages,
+                                                                    visibilityTimeout, _waitTimeSeconds)
                 if messages != []:
                     break
                 time.sleep(self.COOLDOWN_BETWEEN_RECEIVEMESSAGES)
 
         result = []
-        if visibilityTimeout == None:
+        if visibilityTimeout is None:
             visibilityTimeout = int(self.VisibilityTimeout)
+
         for item in messages:
             message = SQSMessage(item['Body'])
             message.id = item['MessageId']
@@ -86,6 +90,58 @@ class SQSQueue:
             result.append(message)
         return result
 
+    def messages(self, attributes='All', visibilityTimeout=None, autodelete=True, neverfail=True):
+        """
+        Generator to retrieve infinitely messages from a queue
+        @type visibilityTimeout: int
+        @type waitTimeSeconds: int
+        @param autodelete: the message deletion will be attempted upon release
+        @type autodelete: bool
+        @param neverfail: don't fail on aws exceptions
+        @type neverfail: bool
+        @rtype: SQSMessage
+        """
+        while True:
+            try:
+                items = self.sqsservice.sqsclient.receiveMessage(self.qName, attributes,
+                                                                 maxNumberOfMessages = 1,
+                                                                 visibilityTimeout = visibilityTimeout,
+                                                                 waitTimeSeconds = 20)
+            except Exception as e:
+                if neverfail:
+                    self.logger.warn('receiveMessage error %s [%s]', message, e)
+                    time.sleep(self.COOLDOWN_BETWEEN_RECEIVEMESSAGES)
+                    continue
+                raise
+
+            if items == []:
+                time.sleep(self.COOLDOWN_BETWEEN_RECEIVEMESSAGES)
+                continue
+
+            item = items[0]
+
+            message = SQSMessage(item['Body'])
+            message.id = item['MessageId']
+            message.receiptHandle = item['ReceiptHandle']
+            message.queue = self
+            message.visibilityTimeout = visibilityTimeout
+            message.receptionTimestamp = time.time()
+            for attribute in item['Attribute']:
+                setattr(message, attribute['Name'], attribute['Value'])
+
+            yield message
+
+            if autodelete:
+                timeleft = message.visibilityTimeoutLeft()
+                if timeleft < 0:
+                    self.logger.warn('Missed the message deletion by %s s. %s', -1*timeleft, message)
+                else:
+                    try:
+                        self.delete(message)
+                    except Exception as e:
+                        if neverfail:
+                            self.logger.warn("Could not delete the message %s [%s]", message, e)
+                        raise
 
     def __repr__(self):
         return "SQSQueue: " + repr(self.__dict__)
