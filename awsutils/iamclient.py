081a7a0456e942ba8c2bc7fc0c54e64b7344d3c6
sanyi <sanyi@fake.com>
2013-02-27 16:24:45 +0200

    + added explicit error handling

--- awsutils/iamclient.py
+++ awsutils/iamclient.py
@@ -6,6 +6,8 @@
 
 from awsutils.awsclient import AWSClient
 from awsutils.utils.auth import SIGNATURE_V2
+import awsutils.exceptions.sqs
+from awsutils.exceptions.aws import extractExceptionsFromModule2Dicitonary
 
 class IAMClient(AWSClient):
     def __init__(self, access_key, secret_key):
@@ -19,3 +21,17 @@ class IAMClient(AWSClient):
             query['UserName'] = userName
         data = self.request(method="GET", signmethod=SIGNATURE_V2, query=query)
         return data['awsresponse']['GetUserResponse']['GetUserResult']['User']
+
+
+    #================================== helper functionality ===========================================================
+
+    EXCEPTIONS = extractExceptionsFromModule2Dicitonary('awsutils.exceptions.sqs',
+                                                        awsutils.exceptions.sqs.SQSException)
+
+    def checkForErrors(self, awsresponse, httpstatus, httpreason, httpheaders):
+        if 'ErrorResponse' in awsresponse:
+            error = awsresponse['ErrorResponse']['Error']
+            if error['Code'] in self.EXCEPTIONS:
+                raise self.EXCEPTIONS[error['Code']](awsresponse, httpstatus, httpreason, httpheaders)
+            else:
+                raise awsutils.exceptions.sqs.SQSException(awsresponse, httpstatus, httpreason, httpheaders)
